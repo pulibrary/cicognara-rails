a5ec6a9a37dfec93d383e5c6138642eff004f493
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-09-02 11:52:02 -0400

    uses single application layout for catalog and catalogo views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  layout 'blacklight'
+  layout 'application'
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,4 +1,3 @@
 HighVoltage.configure do |config|
   config.routes = false
-  config.layout = 'catalogo_section'
 end
