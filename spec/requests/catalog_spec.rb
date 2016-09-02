96aa7a9a882c328c0afd383e666dd3a063a74b02
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-09-02 15:08:10 -0400

    allow tei item catalog view to render when marc record is missing

--- spec/requests/catalog_spec.rb
+++ spec/requests/catalog_spec.rb
@@ -31,6 +31,21 @@ RSpec.describe 'CatalogController config', type: :request do
       get solr_document_path('15')
       expect(response.body).to have_link('Catalogo Item 15', href: '/catalogo/section_1.1/index.html#item_15')
     end
+    describe 'when solr doc from dc_lib is not found' do
+      it 'dclib marc record is not indexed' do
+        get solr_document_path('cico:6gq')
+        expect(response).to have_http_status(404)
+      end
+      it 'catalog item refers to missing solr doc' do
+        get solr_document_path('66'), as: :json
+        dclib = JSON.parse(response.body)['response']['document']['dclib_s']
+        expect(dclib).to include('cico:6gq')
+      end
+      it 'catalog item renders without error' do
+        get solr_document_path('66')
+        expect(response).to have_http_status(200)
+      end
+    end
   end
   after(:all) { Book.destroy_all }
 end
