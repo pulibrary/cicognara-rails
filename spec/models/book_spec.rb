1e9fd9a766ede551208d642c59b4f33e793a4c9a
Cliff Wulfman <cwulfman@princeton.edu>
2016-10-26 22:27:24 -0400

    passed rubocop.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -33,8 +33,7 @@ RSpec.describe Book, type: :model do
   it 'requires dcnum to be unique' do
     book.save
     # create a new book object with the same dcnum as book: xyz
-    expect {
-      described_class.create digital_cico_number: digital_cico_number
-    }.to raise_error ActiveRecord::RecordNotUnique
+    expect { described_class.create digital_cico_number: digital_cico_number }.to
+    raise_error ActiveRecord::RecordNotUnique
   end
 end
