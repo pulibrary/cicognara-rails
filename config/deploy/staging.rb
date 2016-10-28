ca6fd72cd60f29efe394f1468ebe89a0da82ac1f
Trey Pendragon <tterrell@princeton.edu>
2016-10-28 13:28:55 -0700

    Make the staging server a db role (#180)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'libruby-dev.princeton.edu', user: 'deploy', roles: %w(app web)
+server 'libruby-dev.princeton.edu', user: 'deploy', roles: %w(app web db)
 
 set :default_env, fetch(:default_env).merge('RAILS_ENV' => 'staging', 'URL_PATH' => 'cicognara/')
 set :linked_files, fetch(:linked_files, []).push('db/staging.sqlite3')
