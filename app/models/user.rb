539ae4d02fb7a1dc88eac0409dbb81768b521497
Esmé Cowles <escowles@ticklefish.org>
2016-11-08 09:52:18 -0500

    Google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,14 @@ class User < ActiveRecord::Base
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.find_by(email: data['email'])
+    user = User.create(name: data['name'], email: data['email']) unless user
+    user
+  end
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,11 @@ Rails.application.routes.draw do
     concerns :searchable
   end
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'users/auth/google_oauth2', to: 'users/omniauth_callbacks#passthru', as: :new_user_session
+    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   resources :users
   concern :exportable, Blacklight::Routes::Exportable.new
 
