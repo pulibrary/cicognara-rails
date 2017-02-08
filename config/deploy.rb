a7cf6ec939a7cf33b095411c21e928979bbc084c
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-02-08 18:25:34 -0500

    update marc and tei to catalog v2.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle')
 set :default_env,
     'MARCPATH' => 'public/cicognara.mrx.xml',
     'TEIPATH' => 'public/catalogo.tei.xml',
-    'CATALOGO_VERSION' => 'v1.2'
+    'CATALOGO_VERSION' => 'v2.0'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
