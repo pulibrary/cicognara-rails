d7dd0e856719b6beb3bf69b7f4ab010bd56a3fbe
GriffinJ <jrgriffiniii@gmail.com>
2018-07-13 10:03:10 -0400

    [WIP] Adding the spec for the DeleteProcessor

--- spec/services/figgy_event_processor/delete_processor_spec.rb
+++ spec/services/figgy_event_processor/delete_processor_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-describe FiggyEventProcessor::CreateProcessor do
-  subject(:create_processor) { described_class.new(event) }
+describe FiggyEventProcessor::DeleteProcessor do
+  subject(:delete_processor) { described_class.new(event) }
   let(:id) { "62b69875-8ba0-420a-ba2c-2b46f2c801ca" }
   let(:manifest_url) { "http://localhost:3000/concern/scanned_resources/62b69875-8ba0-420a-ba2c-2b46f2c801ca/manifest" }
   let(:collection_slugs) { ["test-cicognara1-slug"] }
   let(:event) do
     {
       "id" => id,
-      "event" => "CREATED",
+      "event" => "DELETED",
       "manifest_url" => manifest_url,
       "collection_slugs" => collection_slugs
     }
   end
 
   describe '#process' do
-
     let(:label) { "Test Title, The" }
     let(:contributing_library) { ContributingLibrary.create(label: "Contributor 1", uri: "http://localhost.localdomain") }
     let(:owner_system_number) { "1234567a" }
@@ -31,26 +30,30 @@ describe FiggyEventProcessor::CreateProcessor do
         owner_system_number: 'abc123'
       )
     end
+    let(:collection) { Collection.create(slug: collection_slugs.first) }
     let(:marc_file_path) { File.join(File.dirname(__FILE__), '..', '..', 'fixtures', 'cicognara.marc.xml') }
     let(:marcxml) { File.open(marc_file_path) { |f| Nokogiri::XML(f) } }
     let(:book) do
       Book.create(
         digital_cico_number: "cico:m87",
+        collections: [collection],
         marcxml: marcxml
       )
     end
+    let(:iiif_resource) { IIIF::Resource.create(url: manifest_url, book: book) }
 
     before do
       version
+      iiif_resource
     end
 
-    it 'creates IIIF::Resource objects in response to CREATE messages' do
-      expect(book.collections).to be_empty
-      create_processor.process
+    it 'deletes IIIF::Resource objects' do
+      expect(book.iiif_resource).to eq iiif_resource
+
+      delete_processor.process
 
       updated_book = book.reload
-      expect(updated_book.collections.length).to eq(1)
-      expect(updated_book.collections.first.slug).to eq collection_slugs.first
+      expect(updated_book.iiif_resource).to be_nil
     end
   end
 end
