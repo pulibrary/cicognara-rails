df93218895eb86b69ad4067e23b70b15e1fc8a9f
Esmé Cowles <escowles@ticklefish.org>
2018-07-27 10:25:35 -0400

    Using experimental MARC from NGA

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle')
 set :default_env,
     'MARCPATH' => 'public/cicognara.mrx.xml',
     'TEIPATH' => 'public/catalogo.tei.xml',
-    'CATALOGO_VERSION' => 'v2.0'
+    'CATALOGO_VERSION' => 'v2.0a'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
