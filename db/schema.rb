5e98b227c457b51eb079a378c0b5b496ca5cfa07
Eliot Jordan <eliotj@princeton.edu>
2018-12-14 10:36:47 -0600

    Use postgres in development

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "book_subjects", force: :cascade do |t|
+  create_table "book_subjects", id: :serial, force: :cascade do |t|
     t.integer "book_id"
     t.integer "subject_id"
     t.datetime "created_at", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["subject_id"], name: "index_book_subjects_on_subject_id"
   end
 
-  create_table "bookmarks", force: :cascade do |t|
+  create_table "bookmarks", id: :serial, force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_type"
     t.string "document_id"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
-  create_table "books", force: :cascade do |t|
+  create_table "books", id: :serial, force: :cascade do |t|
     t.string "marcxml"
     t.string "digital_cico_number"
     t.datetime "created_at", null: false
@@ -53,14 +53,14 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["entry_id"], name: "index_comments_on_entry_id"
   end
 
-  create_table "contributing_libraries", force: :cascade do |t|
+  create_table "contributing_libraries", id: :serial, force: :cascade do |t|
     t.string "label"
     t.string "uri"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "creator_roles", force: :cascade do |t|
+  create_table "creator_roles", id: :serial, force: :cascade do |t|
     t.integer "book_id"
     t.integer "creator_id"
     t.integer "role_id"
@@ -71,14 +71,14 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["role_id"], name: "index_creator_roles_on_role_id"
   end
 
-  create_table "creators", force: :cascade do |t|
+  create_table "creators", id: :serial, force: :cascade do |t|
     t.string "label"
     t.string "uri"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "entries", force: :cascade do |t|
+  create_table "entries", id: :serial, force: :cascade do |t|
     t.string "section_head"
     t.string "section_display"
     t.string "section_number"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["n_value"], name: "index_entries_on_n_value"
   end
 
-  create_table "entry_books", force: :cascade do |t|
+  create_table "entry_books", id: :serial, force: :cascade do |t|
     t.integer "book_id"
     t.integer "entry_id"
     t.datetime "created_at", null: false
@@ -100,14 +100,14 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["entry_id"], name: "index_entry_books_on_entry_id"
   end
 
-  create_table "roles", force: :cascade do |t|
+  create_table "roles", id: :serial, force: :cascade do |t|
     t.string "label"
     t.string "uri"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "searches", force: :cascade do |t|
+  create_table "searches", id: :serial, force: :cascade do |t|
     t.text "query_params"
     t.integer "user_id"
     t.string "user_type"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
-  create_table "subjects", force: :cascade do |t|
+  create_table "subjects", id: :serial, force: :cascade do |t|
     t.string "label"
     t.string "uri"
     t.string "genre"
@@ -128,7 +128,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -147,7 +147,7 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "versions", force: :cascade do |t|
+  create_table "versions", id: :serial, force: :cascade do |t|
     t.integer "contributing_library_id"
     t.integer "book_id"
     t.string "owner_call_number"
@@ -169,5 +169,14 @@ ActiveRecord::Schema.define(version: 20180713162431) do
     t.index ["contributing_library_id"], name: "index_versions_on_contributing_library_id"
   end
 
+  add_foreign_key "book_subjects", "books"
+  add_foreign_key "book_subjects", "subjects"
   add_foreign_key "comments", "entries"
+  add_foreign_key "creator_roles", "books"
+  add_foreign_key "creator_roles", "creators"
+  add_foreign_key "creator_roles", "roles"
+  add_foreign_key "entry_books", "books"
+  add_foreign_key "entry_books", "entries"
+  add_foreign_key "versions", "books"
+  add_foreign_key "versions", "contributing_libraries"
 end
