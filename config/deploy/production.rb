080220832326c01edaf294dcc87a7cb811b5deae
Trey Pendragon <tpendragon@princeton.edu>
2019-11-19 10:24:29 -0800

    Add cicognara2 to Production deploy.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server 'cicognara1.princeton.edu', user: 'deploy', roles: %w(app db web)
+server 'cicognara2.princeton.edu', user: 'deploy', roles: %w(app db web)
 set :deploy_to, '/opt/cicognara/'
 set :default_env, fetch(:default_env).merge('RAILS_ENV' => 'production')
