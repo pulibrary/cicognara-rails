66a0b334c3350a2400259a3bb618c551577c1a95
Clifford Wulfman <cwulfman@princeton.edu>
2019-11-21 11:37:03 -0500

    Adds tests for one-version and no-version conditions
    Co-authored-by: Anna Headley <anna3LC@gmail.com>

--- spec/views/catalog/_version_tabs_default.html.erb_spec.rb
+++ spec/views/catalog/_version_tabs_default.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'catalog/_version_tabs_default.html.erb' do
-  let(:book_record) { Book.new('id' => 'book_record_digital_cico_number') }
+  let(:book_record) { Book.new }
   let(:version1) do
     Version.new(
       id: 1,
       book_id: book_record.id,
       manifest: 'manifest1',
-      imported_metadata: { 'tei_title_txt' => 'First title' }
+      imported_metadata: { 'title_display' => 'First title' }
     )
   end
 
@@ -16,7 +16,7 @@ RSpec.describe 'catalog/_version_tabs_default.html.erb' do
       id: 2,
       book_id: book_record.id,
       manifest: 'manifest2',
-      imported_metadata: { 'tei_title_txt' => 'Title 2' }
+      imported_metadata: { 'title_display' => 'Title 2' }
     )
   end
 
@@ -29,17 +29,51 @@ RSpec.describe 'catalog/_version_tabs_default.html.erb' do
     )
   end
 
-  before do
-    stub_blacklight_views
-    allow(Version).to receive(:where).with(book_id: book_record.id).and_return([version1, version2])
-    allow(Book).to receive(:where).with(digital_cico_number: book_solr_document['id']).and_return([book_record])
+  context 'The book has no versions' do
+    before do
+      stub_blacklight_views
+      allow(Version).to receive(:where).with(book_id: book_record.id).and_return([])
+      allow(Book).to receive(:where).with(digital_cico_number: book_solr_document['id']).and_return([book_record])
+    end
+
+    it 'shows a metadata box for the book' do
+      assign(:linked_books, [book_solr_document])
+      render
+      expect(rendered).to match(/book title display/)
+      expect(rendered).to have_selector('div#book_digital_cico_number')
+    end
   end
 
-  it 'shows a metadata box for each version' do
-    assign(:linked_books, [book_solr_document])
-    render
-    expect(rendered).to have_selector('div#version_1')
-    expect(rendered).to have_selector('div#version_2', visible: false)
-    expect(rendered).to match(/First title/)
+  context 'The book has one version' do
+    before do
+      stub_blacklight_views
+      allow(Version).to receive(:where).with(book_id: book_record.id).and_return([version1])
+      allow(Book).to receive(:where).with(digital_cico_number: book_solr_document['id']).and_return([book_record])
+    end
+
+    it 'shows a metadata box for one version and hides the box for the book' do
+      assign(:linked_books, [book_solr_document])
+      render
+      expect(rendered).to have_selector('div#version_1')
+      expect(rendered).to match(/First title/)
+      expect(rendered).to have_selector('div#book_digital_cico_number', visible: false)
+    end
+  end
+
+  context 'The book has two versions' do
+    before do
+      stub_blacklight_views
+      allow(Version).to receive(:where).with(book_id: book_record.id).and_return([version1, version2])
+      allow(Book).to receive(:where).with(digital_cico_number: book_solr_document['id']).and_return([book_record])
+    end
+
+    it 'shows a metadata box for one version and hides the rest' do
+      assign(:linked_books, [book_solr_document])
+      render
+      expect(rendered).to have_selector('div#version_1')
+      expect(rendered).to have_selector('div#version_2', visible: false)
+      expect(rendered).to match(/First title/)
+      expect(rendered).to have_selector('div#book_digital_cico_number', visible: false)
+    end
   end
 end
