877e66ff1cb3e4adcf887463b998fbdd9d903751
Trey Pendragon <tpendragon@princeton.edu>
2020-08-17 11:02:26 -0700

    Extract switched_to_guided_search

--- app/helpers/advanced_helper.rb
+++ app/helpers/advanced_helper.rb
@@ -26,9 +26,13 @@ module AdvancedHelper
 
   # carries over original search field and original guided search fields if user switches to guided search from regular search
   def guided_field(field_num, default_val)
-    return search_fields_for_advanced_search[params[:search_field]].key || default_val if field_num == :f1 && params[:f1].nil? && params[:f2].nil? && params[:f3].nil? && params[:search_field] && search_fields_for_advanced_search[params[:search_field]]
+    return search_fields_for_advanced_search[params[:search_field]].key || default_val if switched_to_guided_search?
 
     params[field_num] || default_val
+  end
+
+  def switched_to_guided_search?
+    field_num == :f1 && params[:f1].nil? && params[:f2].nil? && params[:f3].nil? && params[:search_field] && search_fields_for_advanced_search[params[:search_field]]
   end
 
   # carries over guided search operations if user switches back to guided search from regular search
